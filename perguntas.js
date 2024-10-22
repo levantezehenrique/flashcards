89babe759ace3aedd76fd8aaccc0ac18f521d129
levantezehenrique <00001076081691sp@al.educacao.sp.gov.br>
2024-10-22 08:28:53 -0300

    projeto de flashcards.

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+
+
+criaCartao(
+    'Futebol Asiático',
+    'Qual jogador marcante do Barcelona atuou no Japão?',
+    'Andrés Iniesta.'
+)
+
+criaCartao(
+    'Eurocopa',
+    'Que país conquistou a penúltima Eurocopa?',
+    'Itália.'
+)
+
+criaCartao(
+    'Copa América',
+    'Que país conquistou a penúltima Copa América?',
+    'Argentina.'
+)
